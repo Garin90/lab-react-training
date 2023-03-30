f039029f8e1b10c4caecf4951d16b1ce358da996
Alex Garrido Baldrich <alex.garrido.baldrich@gmail.com>
2023-03-30 12:24:28 +0200

    iteration 8

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -1,1 +1,36 @@
-//Starting
+import { useState } from 'react';
+
+function LikeButton() {
+    const [counter, setCounter] = useState(0);
+    
+    function handleOnClick() {
+        setCounter((prev) => prev+1);
+    }
+
+    const handleColorButton = () => {
+
+switch (counter) {
+    case 0:
+        return 'bg-success'        
+        break;
+    case 1:
+        return 'bg-danger'        
+        break;
+    case 2:
+        return 'bg-secondary'        
+        break;
+    case 3:
+    return 'bg-primary'        
+    break;
+    default:
+        return ''
+        break;
+}
+
+    }
+
+
+    return <button className={handleColorButton()} onClick={handleOnClick}>{counter} Likes</button>
+}
+
+export default LikeButton;
