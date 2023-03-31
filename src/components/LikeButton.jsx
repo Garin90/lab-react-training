ec305c60dee1ae66a82ad2ecf66a2f61382e077a
Alex Garrido Baldrich <alex.garrido.baldrich@gmail.com>
2023-03-31 11:27:44 +0200

    iteration 8 bonus

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -2,35 +2,22 @@ import { useState } from 'react';
 
 function LikeButton() {
     const [counter, setCounter] = useState(0);
+    const btnStyle = ['purple','blue','green','yellow','orange','red'];
     
     function handleOnClick() {
         setCounter((prev) => prev+1);
     }
 
     const handleColorButton = () => {
+        let laps = Math.floor((counter)/6);
+        const index = counter - laps * 6;
+        return btnStyle[index];
 
-switch (counter) {
-    case 0:
-        return 'bg-success'        
-        break;
-    case 1:
-        return 'bg-danger'        
-        break;
-    case 2:
-        return 'bg-secondary'        
-        break;
-    case 3:
-    return 'bg-primary'        
-    break;
-    default:
-        return ''
-        break;
-}
 
     }
 
 
-    return <button className={handleColorButton()} onClick={handleOnClick}>{counter} Likes</button>
+    return <button style={{backgroundColor: handleColorButton()}} onClick={handleOnClick}>{counter} Likes</button>
 }
 
 export default LikeButton;
